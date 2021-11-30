4b676509efb1fbff7ab16de88957520eb7d60ef8
有希望的男人 <shijiunv@hotmail.com>
2021-11-30 23:52:40 +0800

    添加core注释

--- behave/helper.py
+++ behave/helper.py
@@ -1,35 +1,58 @@
 # coding=utf-8
 
-from .core import BeNode, BeGeneratorAction, BeAction, BeCondition, \
-                        BeSequence, BeSelect, BeDecorator, SUCCESS
+from .core import (
+    BeNode,
+    BeGeneratorAction,
+    BeAction,
+    BeCondition,
+    BeSequence,
+    BeSelect,
+    BeDecorator,
+    SUCCESS,
+)
 import inspect
 from functools import wraps
 
 
 # Functions for testing
 
+
 def is_node(node):
     return isinstance(node, BeNode)
+
 
 def is_sequence(node):
     return isinstance(node, BeSequence)
 
+
 def is_selector(node):
     return isinstance(node, BeSelect)
+
 
 def is_decorator(node):
     return isinstance(node, BeDecorator)
 
+
 def is_condition(node):
     return isinstance(node, BeCondition)
+
 
 def is_action(node):
     return isinstance(node, BeAction) or isinstance(node, BeGeneratorAction)
 
 
-# Decorators for tree node definition
+# 用于tree node定义的装饰器
+
 
 def action(f):
+    """动作节点装饰器
+
+    Args:
+        f: 函数
+
+    Returns:
+        [type]: [description]
+    """
     if inspect.isgeneratorfunction(f):
         return BeGeneratorAction(f)
     else:
@@ -43,12 +66,15 @@ def condition(f):
 def generator_decorator(f):
     def ctor(bb, node):
         g = f(bb, node)
+
         def iter_func():
             try:
                 return next(g)
             except StopIteration:
                 return SUCCESS
+
         return iter_func
+
     return ctor
 
 
@@ -56,4 +82,3 @@ def decorator(f):
     if inspect.isgeneratorfunction(f):
         f = wraps(f)(generator_decorator(f))
     return BeDecorator([f])
-
